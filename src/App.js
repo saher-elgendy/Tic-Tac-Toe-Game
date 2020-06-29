9d2914bae74a5844668232641461b0d9c96468d0
unknown <saherelgendy1990@gmail.com>
2020-06-29 02:22:34 -0700

    fix equal sign

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Game from './components/game';
 
 function App() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
-  const[step, setStep] - useState(0);
+  const [step, setStep] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
-  
+
   return (
     <div className="App">
       <Game />
