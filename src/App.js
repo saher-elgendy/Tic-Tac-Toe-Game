6cc9197fdef2ad6ee827a71dd35be506b6c63916
unknown <saherelgendy1990@gmail.com>
2020-06-29 02:21:20 -0700

    setting the initial state of the game

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Game from './components/game';
 
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const[step, setStep] - useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  
   return (
     <div className="App">
       <Game />
