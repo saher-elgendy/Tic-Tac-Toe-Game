a8237230708a041386a2ca46e2e048e181d8638c
unknown <saherelgendy1990@gmail.com>
2020-06-29 02:45:06 -0700

    adding handleClick function logic

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import Game from './components/game';
 
 function App() {
   const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
-  const [stepNumber, setStep] = useState(0);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXIsNext] = useState(true);
 
+  const handleClick = (id) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const current = newHistory[newHistory.length - 1];
+    const squares = [...current.squares];
+    squares[id] = xIsNext ? 'X' : 'O';
+
+    setHistory([...newHistory, squares]);
+    setXIsNext(!xIsNext);
+    setStepNumber(stepNumber + 1);
+  }
 
   const current = history[stepNumber];
   const squares = current.squares;
   return (
     <div className="App">
-      <Game squares={squares}/>
+      <Game squares={squares} handleClick={handleClick} />
     </div>
   );
 }
