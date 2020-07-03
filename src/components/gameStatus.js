bbb6879b99caf19c29aa1b6996cfae0060139e13
unknown <saherelgendy1990@gmail.com>
2020-07-02 17:33:31 -0700

    check game end in game status

--- src/components/gameStatus.js
+++ src/components/gameStatus.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 
-function GameStatus({ xTurn }) {
+function GameStatus({ xTurn, gameEnd }) {
     return (
         <div className="game-status">
             <p>
-                {xTurn ? 'player X turn' : 'player O turn'}
+                {gameEnd ? 'player ' + (xTurn ? 'O' : 'X') + ' wins' :
+                    (xTurn ? 'player X turn' : 'player O turn')}
             </p>
         </div>
     )
