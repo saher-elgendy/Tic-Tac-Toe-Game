7e44d9fb2b00858b2241c11891708ca1ee84df42
unknown <saherelgendy1990@gmail.com>
2020-07-02 18:31:47 -0700

    adding dynamic style to game status

--- src/components/gameStatus.js
+++ src/components/gameStatus.js
@@ -4,7 +4,9 @@ import React from 'react';
 function GameStatus({ xTurn, gameEnd }) {
     return (
         <div className="game-status">
-            <p>
+            <p style={{
+                fontWeight: gameEnd ? 'bold': 'normal'
+            }}>
                 {gameEnd ? 'player ' + (xTurn ? 'O' : 'X') + ' wins' :
                     (xTurn ? 'player X turn' : 'player O turn')}
             </p>
